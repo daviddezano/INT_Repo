52160e372ce28676c32ce328fa0551bdf42a0666
ddezano <david.dezano@harmonicinc.com>
2024-11-09 19:30:22 +0200

    Ex C is done

--- Lesson11/B/app.py
+++ Lesson11/B/app.py
@@ -10,6 +10,21 @@ def main():
     return render_template("index.html", title="L11 Exercise B")
 
 
+@app.route("/check_tld")
+def check_tld():
+    url = request.args.get('url')
+    tld = url.split(".")[-1]
+    try:
+        resp = requests.get(url = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt", timeout=3)
+        if resp.status_code != 200:
+            raise requests.exceptions.RequestException
+        tlds = resp.text.splitlines()
+        if tld.upper() not in tlds:
+            return {"valid": "INVALID URL FORMAT"}
+    except requests.exceptions.RequestException:
+        return {"valid": "INVALID URL FORMAT"}
+    return {"valid": "VALID"}
+
 @app.route("/check_url")
 def get_url():
     url = request.args.get('url')
--- Lesson11/B/static/script.js
+++ Lesson11/B/static/script.js
@@ -12,11 +12,25 @@ async function checkUrl(url){
     Results.textContent = `URL: '${url}' is '${data.status}'`;
 }
 
+async function validUrl(url){
+    console.log("validate url");
+    const response = await fetch(`/check_tld?url=${url}`);
+    const data = await response.json();
+    console.log(data)
+    console.log(data.valid)
+    if (data.valid != 'VALID'){
+        document.getElementById('url').value = '';
+        alert(data.valid);
+    }
+}
+
+
 Form.addEventListener('submit', function (event) {
     console.log('Form is submitted!');
     event.preventDefault();
     const url = document.getElementById('url').value;
     console.log(`URL to check: ${url}`);
+    valid = validUrl(url);
 
     answer = checkUrl(url);
 });
